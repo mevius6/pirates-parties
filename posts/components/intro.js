228caf9f43691c255bfdb4de9926a6a840d84608
Nicolai Ivanov <iplexus@icloud.com>
2024-01-27 05:33:10 +0300

    monorepo

---
+++ posts/components/intro.js
@@ -0,0 +1,12 @@
+export default function Intro() {
+  return (
+    <section className="intro">
+      <h1 className="title">
+        ХочуТорт
+      </h1>
+      <h4 className="subhead">
+        Блог
+      </h4>
+    </section>
+  )
+}
