b08c340fb35018685d8f4cf352b7bd67cde0c385
Nicolai Ivanov <iplexus@icloud.com>
2024-03-07 07:24:22 +0300

    upd

---
+++ posts/components/nav.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Nav({ navItems }) {
+  return (
+    <div>
+      {/* <h3 className="text-3xl mb-4">
+        <Link href={`https://piratykaspiyskogo.online${navItem.path}`} className="hover:text-brown transition-color duration-150 ease-out hover:ease-in;">{navItem.title}</Link>
+      </h3> */}
+      {/* <p className="text-lg leading-normal mb-5 font-serif">…</p> */}
+    </div>
+  );
+}
