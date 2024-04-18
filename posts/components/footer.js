1e794207ac17c82076eae9b450df0d64d59f64f0
Nicolai Ivanov <iplexus@icloud.com>
2024-04-18 22:31:43 +0300

    upd footer

--- posts/components/footer.js
+++ posts/components/footer.js
@@ -6,12 +6,12 @@ export default function Footer() {
   return (
     <footer className="bg-lime border-t border-black">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tight leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2 uppercase text-navy">
-            {/* {BRAND_NAME} */}
-            {/* <small className="block font-serif pt-5">© 2024</small> */}
-            <Logo width={"100%"} />
-          </h3>
+        <div className="py-28 flex flex-col lg:flex-row items-center justify-center">
+          <Logo width={"50%"} />
+          {/* <h3 className="text-4xl lg:text-5xl font-bold tracking-tight leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2 uppercase text-navy">
+            {BRAND_NAME}
+            <small className="block font-serif pt-5">© 2024</small>
+          </h3> */}
           {/* <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="/"
